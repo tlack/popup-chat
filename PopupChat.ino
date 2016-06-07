debddbcdc4f5ec39c11be05b73848ac702b60880
tlack <lackner@gmail.com>
2016-06-06 21:22:13 -0400

    first checkin of PopupChat

---
+++ PopupChat.ino
@@ -0,0 +1,114 @@
+// PopupChat - a one-screen system to share things locally with your friends via WIFI
+// based on: Captive Portal by: M. Ray Burnette 20150831
+// homo est bulae
+#include <ESP8266WiFi.h>
+#include "./DNSServer.h"                  // Patched lib
+#include <ESP8266WebServer.h>
+
+// config
+#define CHATNAME "Instant Anonymous Chatroom"
+#define BLURB "Experimental local area chat"
+#define COMPLAINTSTO "tlack"
+#define INDEXTITLE "Howdy friend!"
+#define INDEXBANNER "This is a local-only, non-cloud chat room, that you use through your phone's web browser. Messages are deleted every now and then. Have fun and be nice to each other! <a href=/faq>See frequently asked questions..</a>"
+#define POSTEDTITLE "Message posted!"
+#define POSTEDBANNER "OK, you should be good to go. Your message will stay live for a short time - perhaps a couple of days at most, until the wee server is rebooted. Here it is again:"
+const String FAQ = "this is an experiment at making a chat room that only works within the wifi range of a small physical device with no hard drive (called an Arduino)<br/>"
+"you can't use the web on this wifi access point, but you can use this message board.<br/>"
+"it's completely anonymous and only the last few messages are kept. nothing is saved permanently.<br/>"
+"this software is very new. please be patient with bugs. and don't be a jerk.<br/>";
+
+// boring
+#define VER "@tlack/popup R0"
+const byte HTTP_CODE = 200; // nyi? 511; // rfc6585
+const byte DNS_PORT = 53;  // Capture DNS requests on port 53
+const byte TICK_TIMER = 1000;
+const byte ACTIVITY_DURATION = 60 * TICK_TIMER; // how many seconds should the LED stay on after last visit?
+const byte ACTIVITY_LED = 2;
+const byte ACTIVITY_REVERSE = 1; // turn off when active, not on.. needed for me
+IPAddress APIP(10, 10, 10, 1);    // Private network for server
+// state:
+String allMsgs="<i>*system restarted*</i>";
+unsigned long bootTime=0, lastActivity=0, lastTick=0, tickCtr=0; // timers
+DNSServer dnsServer; ESP8266WebServer webServer(80); // standard api servers
+void em(String s){ Serial.print(s); } 
+void emit(String s){ Serial.println(s); } // debugging
+String input(String argName) {
+  String a=webServer.arg(argName);
+  a.replace("<","&lt;");a.replace(">","&gt;");
+  a.substring(0,200); return a; }
+String quote() { 
+  const byte nquotes=3;
+  String quotes[nquotes]={
+    "Blushing is the color of virtue -Diogenes of Synope", 
+    "A bear can run as fast as a horse",
+    "Homo homini lupus est - man is a wolf to fellow man",
+  };
+  return quotes[millis() / 1000 / 60 / 60 % nquotes];
+}
+String footer() { return 
+  "</div><div class=q><label>Quote of the hour:</label>"+quote()+"</div>"
+  "<div class=com>Complaints to: " COMPLAINTSTO "</div>"
+  "<div class=by>" VER "</div></body></html>"; }
+String header(String t) {
+  String a = String(CHATNAME);
+  String CSS = "article { background: #f2f2f2; padding: 1em; }" 
+    "body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
+    "div { padding: 0.5em; }"
+    "h1 { margin: 0.5em 0 0 0; padding: 0; }"
+    "input { border-radius: 0; }"
+    "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
+    "nav { background: #eb3570; color: #fff; display: block; font-size: 1.3em; padding: 1em; }"
+    "nav b { display: block; font-size: 1.2em; margin-bottom: 0.5em; } "
+    "textarea { width: 100%; }";
+  String h = "<!DOCTYPE html><html>"
+    "<head><title>"+a+" :: "+t+"</title>"
+    "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
+    "<style>"+CSS+"</style></head>"
+    "<body><nav><b>"+a+"</b> "+BLURB+"</nav><div><h1>"+t+"</h1></div><div>";
+  emit("header - "+t);
+  emit(h);
+  return h; }
+String faq() {
+  return header("frequently asked questions") + FAQ + footer();
+}
+String index() {
+  return header(INDEXTITLE) + "<div>" + INDEXBANNER + "</div><div><label>Last few messages:</label><ol>"+allMsgs+
+    "</ol></div><div><form action=/post method=post><label>Post new message:</label><br/>"+
+    "<i>remember:</i> include your name or something like it</i><br/>"+
+    "<textarea name=m></textarea><br/><input type=submit value=send></form>" + footer();
+}
+String posted() {
+  String msg=input("m"); allMsgs="<li>"+msg+"</li>"+allMsgs;
+  emit("posted: "+msg); 
+  return header(POSTEDTITLE) + POSTEDBANNER + "<article>"+msg+"</article><a href=/>Back to index</a>" + footer();
+}
+void setup() {
+  Serial.begin(115200); 
+  emit("setup"); 
+  bootTime = lastActivity = millis();
+  pinMode(ACTIVITY_LED, OUTPUT); led(1);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(APIP, APIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP(CHATNAME);
+  dnsServer.start(DNS_PORT, "*", APIP);
+  webServer.on("/post",[]() { webServer.send(HTTP_CODE, "text/html", posted()); });
+  webServer.on("/faq",[]() { webServer.send(HTTP_CODE, "text/html", faq()); });
+  webServer.onNotFound([]() { lastActivity=millis(); webServer.send(HTTP_CODE, "text/html", index()); });
+  webServer.begin();
+}
+void led(byte p){
+  byte on=p^ACTIVITY_REVERSE; emit("led"+String(on));
+  digitalWrite(ACTIVITY_LED, on ? HIGH : LOW);
+}
+void tick() {
+  String tickCs=String(tickCtr++); // emit("tick #"+tickCs+" @"+String(millis()));
+  if ((millis() - lastActivity) < ACTIVITY_DURATION) {
+    em("+"); led(1);
+  } else {
+    em("-"); lastActivity = 0; led(0);
+  }
+}
+void loop() { 
+  if ((millis()-lastTick)>TICK_TIMER) {lastTick=millis(); tick();} 
+  dnsServer.processNextRequest(); webServer.handleClient(); }
---
+++ README.md
@@ -0,0 +1,83 @@
+# PopupChat 
+
+PopupChat is an open anonymous instant chat room.
+
+![Screenshot of PopupChat Arduino instant chat room](http://www.modernmethod.com/send/files/popupchatscreenshot.jpg)
+
+PopupChat is an Arduino sketch that turns your ESP8266 into an open wifi access
+point that connects users to an anonymous chat room.  The user is (usually)
+sent directly into a popup web browser with the software open as soon as they
+connect to the wireless AP, because it acts like a Captive Portal (think "hotel
+wifi").
+
+## Ethics
+
+PopupChat features impermanence. All data is temporary, and PopupChat maintains
+no logs. The entire system is reset to its default state when power is cycled. 
+
+## Purpose
+
+I wanted to explore the ESP8266 and I'm fascinated by the opportunities that
+highly connected low cost devices like the ESP8266 (and hopefully ESP32)
+engender.
+
+Data points:
+
+- It's conjectured that the ESP8266 can run for up to 17 days on merely a watch
+	battery
+- The chip costs $2 in bulk, or $10 for a deee-luxe development board
+- Not much of an operating system to cause confusing behavior
+- Single-purposes devices theoretically enable a smaller attack surface for
+	security attacks (in practice vendor bugs probably mitigate the benefit here..
+	note ESP8266 buggy TLS)
+
+## Inspiration
+
+I miss the hyper local and easy to operate bulletin board systems of my youth.
+My favorite part was checking the Wall to see who's logged on and left a message.
+Similar to the old fingerd protocol, it was way easier to communicate 
+
+## Scenarios for usage
+
+All kinds of adhoc communication and group chat systems could be assisted by
+software like this running on low-cost boards like the ESP8266.
+
+- Disaster relief/mountain rescue organization, triage, etc.
+- On-premises customer chat
+- Community bulletin board
+- Public soapbox
+
+## Current status
+
+- *Pretty bare bones*. Just a proof of concept.
+- I don't think I nailed the Android Captive Portal experience. Doesn't seem to
+	pop up the browser. Some more useful info here:
+	http://serverfault.com/questions/679393/captive-portal-popups-the-definitive-guide
+- Needs great JS UI with autoupdate in order to feel like a finished product.
+- Would be best rewritten under NodeMCU.
+- Permanency is a valid goal for some situations. PopupChat would benefit from
+	a version that can store things permanently. The ideal interface would be the
+	NodeMCU `file` API.
+- Needs a fine-grained control panel/sysop menu with a way to delete troll
+	droppings.
+- System should maintain some kind of history of its up times and down times so
+	that the sysop can understand issues relating to installation/power/hardware
+	etc. I've experienced a few surprise restarts.
+- Needs dates, especially system reset dates.
+
+## Installation
+
+I used Arduino to make this, but I'd like to rebuild it with NodeMCU.
+
+Edit the configuration at the top of the .ino file and upload it to your 8266. That's about all it should take.
+
+## Provenance/License
+
+MIT license. Based on Captive Portal by M. Ray Burnette 20150831. 
+
+Built at [Building.co](http://building.co) in Miami, FL.
+
+## Contact
+
+@tlack on Twitter
+
